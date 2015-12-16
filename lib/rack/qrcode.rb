57eaf0395b4468969d4ce650c83bee18521fe674
haruyama-makoto <makoto.haruyama@dena.com>
2015-12-16 14:08:38 +0900

    Add qrcode generate

--- lib/rack/qrcode.rb
+++ lib/rack/qrcode.rb
@@ -1,7 +1,36 @@
 require "rack/qrcode/version"
+require 'rqrcode_png'
+require "cgi"
 
 module Rack
-  module Qrcode
-    # Your code goes here...
+  class Qrcode
+    def initialize(app, options = {})
+      @app  = app
+      @path = options[:path] || '/qrcode'
+    end
+
+    def call(env)
+      return @app.call(env) unless env['PATH_INFO'] == @path
+
+      params = CGI.parse(env["QUERY_STRING"])
+      if params['text'].empty?
+        return [400, {'Content-Type' => 'text/html'}, ["Bad Request"]]
+      end
+      text   = params['text'].first
+      size   = params['size'].empty?  ?  4 : params['size'].first.to_i
+      level  = params['level'].empty? ? :h : params['level'].first.to_sym
+      width  = params['width'].empty?  ? 200 : params['width'].first.to_i
+      height = params['height'].empty? ? 200 : params['height'].first.to_i
+      qr  = RQRCode::QRCode.new(text, size: size, level: level)
+      png = qr.to_img
+      body = png.resize(width, height).to_blob
+      headers = {
+        "Content-Length" => body.bytesize.to_s,
+        "Content-Type"   => "imgae/png",
+        "Last-Modified"  => Time.now.httpdate
+      }
+
+      [200, headers, [body]]
+    end
   end
 end
