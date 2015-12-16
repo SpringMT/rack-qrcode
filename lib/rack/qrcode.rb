ff838223575390e64731b45079f915ed19aa57dd
haruyama-makoto <makoto.haruyama@dena.com>
2015-12-16 14:03:22 +0900

    first commit

---
+++ lib/rack/qrcode.rb
@@ -0,0 +1,7 @@
+require "rack/qrcode/version"
+
+module Rack
+  module Qrcode
+    # Your code goes here...
+  end
+end
---
+++ lib/rack/qrcode/version.rb
@@ -0,0 +1,5 @@
+module Rack
+  module Qrcode
+    VERSION = "0.1.0"
+  end
+end
