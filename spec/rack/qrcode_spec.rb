355f78d09ecee7851d292ea03e001d3221235e5e
okazu-dm <uhavetwocows@gmail.com>
2016-01-30 20:49:16 +0900

    Add test case

--- spec/rack/qrcode_spec.rb
+++ spec/rack/qrcode_spec.rb
@@ -1,11 +1,28 @@
 require 'spec_helper'
+require 'application_helper'
+require 'rack/test'
 
 describe Rack::Qrcode do
+  include Rack::Test::Methods
+  
+  let(:app) {Rack::Qrcode.new(ApplicationHelper::Application.new())}
+
   it 'has a version number' do
     expect(Rack::Qrcode::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'return a valid qrcode' do
+    get '/qrcode', {
+        text: 'test qrcode',
+        width: 40,
+        height: 40,
+        size: 8,
+        level: :h
+    }
+    expect(last_response['Content-Type']).to eq 'image/png'
+    expect(last_response['Content-Length'].to_i).to be > 0
+    expect(last_response.status).to eq 200
+    expect(last_response.status).to eq 200
   end
+
 end
