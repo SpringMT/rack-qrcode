a967b7a86ef617670c578e7a29b24d2402bf5358
haruyama-makoto <makoto.haruyama@dena.com>
2016-01-31 12:41:28 +0900

    Fix test

--- rack-qrcode.gemspec
+++ rack-qrcode.gemspec
@@ -31,5 +31,6 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec"
   spec.add_development_dependency "rack-test"
+  spec.add_development_dependency "pry"
   spec.add_dependency 'rqrcode_png'
 end
--- spec/rack/qrcode_spec.rb
+++ spec/rack/qrcode_spec.rb
@@ -1,11 +1,28 @@
 require 'spec_helper'
-require 'application_helper'
+require 'pry'
 require 'rack/test'
 
+module ApplicationHelper
+  extend self
+  class Application
+    def call(env)
+      code   = 200
+      body   = [ "test body" ]
+      header = { "Content-Type"           => "text/html;charset=utf-8",
+                 "Content-Length"         => "9",
+                 "X-XSS-Protection"       => "1; mode=block",
+                 "X-Content-Type-Options" => "nosniff",
+                 "X-Frame-Options"        => "SAMEORIGIN" }
+      [ code, header, body ]
+    end
+  end
+end
+
 describe Rack::Qrcode do
+  include ApplicationHelper
   include Rack::Test::Methods
-  
-  let(:app) {Rack::Qrcode.new(ApplicationHelper::Application.new())}
+
+  let(:app) { Rack::Qrcode.new(ApplicationHelper::Application.new()) }
 
   it 'has a version number' do
     expect(Rack::Qrcode::VERSION).not_to be nil
@@ -13,15 +30,14 @@ describe Rack::Qrcode do
 
   it 'return a valid qrcode' do
     get '/qrcode', {
-        text:  'test qrcode',
-        width:  40,
-        height: 40,
-        size:   8,
-        level:  :h
+      text:  'test qrcode',
+      width:  40,
+      height: 40,
+      size:   8,
+      level:  :h
     }
     expect(last_response['Content-Type']).to eq 'image/png'
     expect(last_response['Content-Length'].to_i).to be > 0
-    expect(last_response.status).to eq 200
     expect(last_response.status).to eq 200
   end
 
